6d404eb6cbf5b2bfffb37dd81aec7586f1604e9a
Leonardo Arbelo <leoarbelo04@gmail.com>
2025-05-27 20:07:31 -0300

    fin cart detail
    diseño listo, falta integracion con paypal

--- screens/CartDetail.js
+++ screens/CartDetail.js
@@ -1,9 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native';
 import axios from 'axios';
 
+import { AuthContext } from '../context/AuthContext';
+import { NavigationContainer, createStaticNavigation, useNavigation } from '@react-navigation/native';
+
+
 export function CartDetail({ route }) {
   const { trip, selectedSeats } = route.params;
+  
+  const navigation = useNavigation();
+  const {login} = useContext(AuthContext);
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#e8ecf4' }}>
@@ -17,17 +24,84 @@ export function CartDetail({ route }) {
           />
         </View>
         <Text style={styles.title}>Detalle de Compra</Text>
-        <Text style={styles.label}>Viaje seleccionado:</Text>
-        
-        <Text style={styles.label}>Asientos seleccionados:</Text>
-        <Text style={styles.label}>{selectedSeats.join(', ')}</Text>
+        <Text style={styles.txtDetail}>
+          <Text style={{fontWeight: 'bold'}}>Origen: </Text>
+          {trip.origen} Montevideo 
+        </Text>
+        <Text style={styles.txtDetail}>
+          <Text style={{fontWeight: 'bold'}}>Destino: </Text>
+          {trip.destino} Canelones
+          </Text>
+        <Text style={styles.txtDetail}>
+          <Text style={{fontWeight: 'bold'}}>Salida: </Text>
+          {trip.fecha_salida} 12/7/2025 18:20hs
+          </Text>
+        <Text style={styles.txtDetail}>
+          <Text style={{fontWeight: 'bold'}}>Llegada: </Text>
+          {trip.fecha_llegada} 12/7/2025 19:30hs
+          </Text>
+        <Text style={styles.txtDetail}>
+          <Text style={{fontWeight: 'bold'}}>Omnibus: </Text>
+          {trip.omnibus} 409
+          </Text>        
+        <Text style={styles.txtDetail}>
+          <Text style={{fontWeight: 'bold'}}>Asientos seleccionados: </Text>
+          {selectedSeats.join(', ')}
+        </Text>
+
+
+
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', marginLeft: '50%', marginTop: 50, marginBottom: 0 }}>
+          <Text style={[styles.txtDetail, { width: 130 }]}>
+            <Text style={{fontWeight: 'bold'}}>SubTotal: </Text>
+          </Text>
+          <Text style={styles.txtDetail}>
+            ${trip.origen} 1500 
+          </Text>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', marginLeft: '50%', marginTop: 0, marginBottom: 10 }}>
+          <Text style={[styles.txtDetail, { width: 130 }]}>
+            <Text style={{fontWeight: 'bold'}}>Descuentos: </Text>
+          </Text>
+          <Text style={styles.txtDetail}>
+            ${trip.origen} 50 
+          </Text>
+        </View>
+        <View style={{ 
+          flexDirection: 'row', 
+          justifyContent: 'flex-start', 
+          marginLeft: '50%', 
+          marginTop: 0, 
+          marginBottom: 0, 
+          borderBottomColor: '#b0b0b0',
+          borderWidth: 1,
+        }}>
+
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start',marginLeft: '50%', marginTop: 0, marginBottom: 100 }}>
+          <Text style={[styles.txtDetail, { width: 130 }]}>
+            <Text style={{fontWeight: 'bold'}}>Total:</Text>
+          </Text>
+          <Text style={styles.txtDetail}>
+            $1450
+          </Text>
+        </View>
+
         <View style={styles.formAction}>
-            <TouchableOpacity 
-            onPress={() => {login(form.email,form.password)}}>
+          <TouchableOpacity 
+            onPress={() => {forgotPass(form.email)}}>
             <View style={styles.btn}>
-                <Text style={styles.btnText}>Comprar con Paypal</Text>
+              <Text style={styles.btnText}>Comprar con Paypal</Text>
             </View>
-            </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.formAction}>
+          <TouchableOpacity
+              onPress={() => navigation.navigate('SearchScreen')}>
+            <View style={styles.btnCancel} >
+              <Text style={styles.btnTextCancel}>Cancelar</Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -85,29 +159,52 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginTop: 20,
     color: '#222',
-    textAlign: 'center',
+    textAlign: 'left',
+  },
+  txtDetail: {
+    fontSize: 20,
+    marginTop: 10,
+    textAlign: 'left',
+    color: '#222',
   },
   btn: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: 30,
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderWidth: 1,
-      backgroundColor: '#075eec',
-      borderColor: '#075eec',
-    },
-    btnText: {
-      fontSize: 25,
-      lineHeight: 26,
-      fontWeight: '1000',
-      color: '#fff',
-    },
-    formAction: {
-      marginTop: 25,
-      marginBottom: 16,
-    },
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: '#075eec',
+    borderColor: '#075eec',
+  },
+  btnText: {
+    fontSize: 25,
+    lineHeight: 26,
+    fontWeight: '1000',
+    color: '#fff',
+  },
+  btnCancel: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 5,
+    backgroundColor: '#FFFFFF',
+    borderColor: '#075eec',
+  },
+  btnTextCancel: {
+    fontSize: 25,
+    lineHeight: 25,
+    fontWeight: '1000',
+    color: '#000000',
+  },
+  formAction: {
+    marginTop: 15,
+    marginBottom: 0,
+  },
 });
 
 export default CartDetail;
